cb87306950f32decd4f80216598128071d83ffee
MoshiMoshi0 <902882+MoshiMoshi0@users.noreply.github.com>
2019-06-06 11:26:22 +0200

    Cleanup warnings (#94)
    * (CS3005) Remove unused duplicate property differing only in case
    
    * (CS3001, CS3003, CS3005) Make OpenHardwareMonitorLib not CLS compliant
    
    * (NU5125) licenseUrl will be deprecated
    
    * (CS0618) SecurityAction.RequestMinimum is obsolete
    
    * "Migrated rules for Aga.Controls.ruleset" is missing

---
+++ TemperatureNotifier/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using OpenHardwareMonitor.Hardware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using SystemFonts = System.Drawing.SystemFonts;
+using Color = System.Drawing.Color;
+using System.ComponentModel;
+
+namespace TemperatureNotifier
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += OnClosingWindow;
+
+            Computer computer = new Computer()
+            {
+                CPUEnabled = true,
+                GPUEnabled = true
+            };
+
+            computer.Open();
+
+            var trays = new Dictionary<string, TemperatureTray>();
+            trays["CPU Package"] = new TemperatureTray(this, SystemFonts.DefaultFont, Color.CadetBlue);
+            trays["GPU Core"] = new TemperatureTray(this, SystemFonts.DefaultFont, Color.Orchid);
+
+
+            Timer timer = new Timer() { Enabled = true, Interval = 5000 };
+            timer.Elapsed += delegate (object sender, ElapsedEventArgs e)
+            {
+                foreach (IHardware hardware in computer.Hardware)
+                {
+                    hardware.Update();
+                    foreach (ISensor sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            if (trays.TryGetValue(sensor.Name, out TemperatureTray tray))
+                            {
+                                tray.ShowText(sensor.Value.ToString()); ;
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+        private void OnClosingWindow(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
+    }
+}
---
+++ TemperatureNotifier/TemperatureTray.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemperatureNotifier
+{
+    class TemperatureTray
+    {
+        private MainWindow mainWindow;
+        private ContextMenu contextMenu;
+        private NotifyIcon notifyIcon;
+        private Font font;
+        private Color color;
+        public TemperatureTray(MainWindow pMainWindow, Font font, Color color)
+        {
+            mainWindow = pMainWindow;
+            this.color = color;
+            this.font = font;
+            notifyIcon = new NotifyIcon();
+            ShowText("0", font, color);
+            notifyIcon.Text = "Tray test";
+            notifyIcon.Visible = true;
+            notifyIcon.DoubleClick += TrayOpenClick;
+
+            contextMenu = new ContextMenu();
+            contextMenu.MenuItems.Add(0,
+                new MenuItem("Odtwórz", new System.EventHandler(TrayOpenClick)));
+            contextMenu.MenuItems.Add(1,
+                new MenuItem("Zamknij", new EventHandler(TrayExitClick)));
+            notifyIcon.ContextMenu = contextMenu;
+        }
+        private void TrayExitClick(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void TrayOpenClick(object sender, EventArgs e)
+        {
+            mainWindow.Show();
+            mainWindow.Activate();
+        }
+        //public void ShowTrayInformation(string Title, string Content)
+        //{
+        //    notifyIcon.BalloonTipTitle = Title;
+        //    notifyIcon.BalloonTipText = Content;
+        //    notifyIcon.BalloonTipIcon = ToolTipIcon.None;
+        //    notifyIcon.Visible = true;
+        //    notifyIcon.ShowBalloonTip(30000);
+
+        //    notifyIcon.BalloonTipClicked += delegate (object sender, EventArgs args)
+        //    {
+        //        mainWindow.Show();
+        //        mainWindow.Activate();
+        //    };
+        //}
+        public void ShowText(string text, Font font, Color col)
+        {
+            Brush brush = new SolidBrush(col);
+
+            // Create a bitmap and draw text on it
+            Bitmap bitmap = new Bitmap(16, 16);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.DrawString(text, font, brush, 0, 0);
+
+            // Convert the bitmap with text to an Icon
+            Icon icon = Icon.FromHandle(bitmap.GetHicon());
+
+            notifyIcon.Icon = icon;
+        }
+
+        public void ShowText(string text)
+        {
+            Brush brush = new SolidBrush(color);
+
+            // Create a bitmap and draw text on it
+            Bitmap bitmap = new Bitmap(16, 16);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.DrawString(text, font, brush, 0, 0);
+
+            // Convert the bitmap with text to an Icon
+            Icon icon = Icon.FromHandle(bitmap.GetHicon());
+
+            notifyIcon.Icon = icon;
+        }
+
+    }
+}
